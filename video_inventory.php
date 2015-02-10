01f88586ade684dadbc3e69101e5465c7a9049a7
greg <gregorio.jp.mitchell@gmail.com>
2015-02-10 14:08:45 -0700

    Wired up 'Add Video' button, decomposed the program into functions, started to handle requests.  Need to figure out connect 'Delete' buttons to sql

--- video_inventory.php
+++ video_inventory.php
@@ -7,11 +7,76 @@
 
 
 <?php
-include './credentials.php';
+
+checkPostRequests();
+addVideoForm();
+$mysqli = connectToDB();
+drawVideos($mysqli);
+
+
+// handle post requests via mysql
+function checkPostRequests(){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') 
+    {
+        if (! empty($_POST))
+            {
+                if ( isset($_POST["name"]) && $_POST["name"] != null && $_POST["name"] != '')
+                    {
+                        if ( isset($_POST["category"]) && $_POST["category"] != null)
+                            {
+                                if ( isset($_POST["length"]) && $_POST["length"] != null)
+                                    {
+                                        // everything is set here. 
+                                        // just need to tell mysql 
+                                        $name = $_POST["name"];
+                                        $category = $_POST["category"];
+                                        $length = $_POST["length"];
+                                        
+                                    }
+                                else // length is NULL.  This is okay.
+                                    {
+                                        $name = $_POST["name"];
+                                        $category = $_POST["category"];
+                                        $length = "";
+                                    }
+                            }
+                        else // category is NULL.  This is okay.
+                            {
+                                if ( isset($_POST["length"]) && $_POST["length"] != null)
+                                    { 
+                                        $name = $_POST["name"];
+                                        $category = "";
+                                        $length = $_POST["length"];
+                                    }
+                                else // length is NULL.  This is okay.
+                                    {
+                                        $name = $_POST["name"];
+                                        $category = "";
+                                        $length = "";
+
+                                    }
+                            }
+
+                        $mysqli = connectToDB();
+                        $sql = "insert into video_inventory ".
+                            "(name,category,length) ".
+                            "values (\"$name\",\"$category\",\"$length\");";
+                        // echo "Trying to add $sql";
+                        $result = $mysqli->query($sql);
+                    }
+                else // name is NULL.  This is NOT okay.
+                    {
+                        echo "You must have a name.";
+                    }
+            } // end check for empty post
+    } // end check if server request is post type 
+}
+
 
 // output a form to add videos
-echo <<<_END
-<form method ='post' action='video_inventory.php'>
+function addVideoForm() {
+    echo <<<_END
+<form method ='post' action='video_inventory.php&add=true'>
 <table name='addBar'>
  <tr><td>name:</td> <td> <input type='text' name='name'></td>
      <td>category:</td> <td> <input type='text' name='category'></td>
@@ -19,7 +84,21 @@ echo <<<_END
      <td colspan='2' align='center'> <input type='submit' value='Add Video'></td></tr>
 </table></form>
 _END;
+}
 
+
+// output a form to delete videos
+function delVideoForm() {
+    echo <<<_END
+<form method ='post' action='video_inventory.php&delete=true'>
+<input type='submit' value='Delete'>
+</form>
+_END;
+}
+
+
+function connectToDB(){
+include './credentials.php';  // username, hostname,password
 $mysqli = new mysqli($hostname, $username, $password, "290_assignment4_video_tracker");
 if ($mysqli->connect_errno)
     {
@@ -27,7 +106,10 @@ if ($mysqli->connect_errno)
             $mysqli->connect_errno. ") " . 
             $mysqli->connect_error;
     }
+return $mysqli;
+}
 
+function drawVideos($mysqli){
 echo "<table border=1><tr><th>id<th>name<th>category<th>length<th>rented</tr>";
 $sql = "select id,name,category,length,rented from video_inventory";
 $result = $mysqli->query($sql);
@@ -39,12 +121,19 @@ if ($result->num_rows>0)
                 echo "<tr><td> " . $row["id"]. "</td>".
                     "<td>" . $row["name"]. "</td>".
                     "<td>". $row["category"]. "</td>".
-                    "<td> " . $row["length"]. "</td>".
-                    "<td>" . $row["rented"]. "</tr>";
+                    "<td> " . $row["length"]. "</td>";
+                    // "<td>" . $row["rented"]. "</tr>";
+                    if ($row["rented"] == 0)
+                        echo "<td>available</td>";
+                    else 
+                        echo "<td>checked out</td>";
+                    echo "<td>";
+                    delVideoForm(); 
+                    echo "</td></tr>";
             }
     }
 echo "</table>";
-
+}
 
 ?>
 
