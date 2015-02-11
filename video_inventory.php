2222983ed38891107e8f2c2dee03d278d38118a3
greg <gregorio.jp.mitchell@gmail.com>
2015-02-10 22:06:10 -0700

    Converted SQL calls to use prepared statements from http://php.net/manual/en/mysqli-stmt.fetch.php.  This might be the wrong driver to use for ONID/FLIP, but it works on ubuntu.

--- video_inventory.php
+++ video_inventory.php
@@ -13,13 +13,74 @@ addVideoForm();
 $mysqli = connectToDB();
 drawVideos($mysqli);
 
+
+// delete a row from the table with the given id
+// the alternate approach is to use hidden inputs, which might be more robust
 function checkDeletions(){
     $id = array_search('Delete',$_POST); // search post array for the id
-    // echo "Try to delete " . $id . "<br>";
     $mysqli = connectToDB();
-    $sql = "delete from video_inventory ".
+    // stage1: prepare
+    $query = "delete from video_inventory ".
         "where id = ".$id;
-    $result = $mysqli->query($sql);
+    if( $sql = $mysqli->prepare($query)){
+        $sql->execute();
+        
+    }
+}
+
+
+function checkInsertions(){
+    if ( isset($_POST["name"]) && $_POST["name"] != null && $_POST["name"] != '')
+        {
+            if ( isset($_POST["category"]) && $_POST["category"] != null)
+                {
+                    if ( isset($_POST["length"]) && $_POST["length"] != null)
+                        {
+                            // everything is set here. 
+                            // just need to tell mysql 
+                            $name = $_POST["name"];
+                            $category = $_POST["category"];
+                            $length = $_POST["length"];
+                            
+                        }
+                    else // length is NULL.  This is okay.
+                        {
+                            $name = $_POST["name"];
+                            $category = $_POST["category"];
+                            $length = "";
+                        }
+                }
+            else // category is NULL.  This is okay.
+                {
+                    if ( isset($_POST["length"]) && $_POST["length"] != null)
+                        { 
+                            $name = $_POST["name"];
+                            $category = "";
+                            $length = $_POST["length"];
+                        }
+                    else // length is NULL.  This is okay.
+                        {
+                            $name = $_POST["name"];
+                            $category = "";
+                            $length = "";
+                            
+                        }
+                }
+            
+            $mysqli = connectToDB();
+            $sql = "insert into video_inventory ".
+                "(name,category,length) ".
+                "values (\"$name\",\"$category\",\"$length\");";
+            // echo "Trying to add $sql";
+            $result = $mysqli->query($sql);
+        }
+    /*
+    else // name is NULL.  This is NOT okay.
+        {
+            echo "You must have a name.";
+        }
+    */
+    
 }
 
 
@@ -29,58 +90,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
     {
         if (! empty($_POST))
             {
-                checkDeletions();
-                if ( isset($_POST["name"]) && $_POST["name"] != null && $_POST["name"] != '')
-                    {
-                        if ( isset($_POST["category"]) && $_POST["category"] != null)
-                            {
-                                if ( isset($_POST["length"]) && $_POST["length"] != null)
-                                    {
-                                        // everything is set here. 
-                                        // just need to tell mysql 
-                                        $name = $_POST["name"];
-                                        $category = $_POST["category"];
-                                        $length = $_POST["length"];
-                                        
-                                    }
-                                else // length is NULL.  This is okay.
-                                    {
-                                        $name = $_POST["name"];
-                                        $category = $_POST["category"];
-                                        $length = "";
-                                    }
-                            }
-                        else // category is NULL.  This is okay.
-                            {
-                                if ( isset($_POST["length"]) && $_POST["length"] != null)
-                                    { 
-                                        $name = $_POST["name"];
-                                        $category = "";
-                                        $length = $_POST["length"];
-                                    }
-                                else // length is NULL.  This is okay.
-                                    {
-                                        $name = $_POST["name"];
-                                        $category = "";
-                                        $length = "";
-                                        
-                                    }
-                            }
-                        
-                        $mysqli = connectToDB();
-                        $sql = "insert into video_inventory ".
-                            "(name,category,length) ".
-                            "values (\"$name\",\"$category\",\"$length\");";
-                        // echo "Trying to add $sql";
-                        $result = $mysqli->query($sql);
-                    }
-                else // name is NULL.  This is NOT okay.
-                    {
-                        echo "You must have a name.";
-                    }
-            } // end check for add video
-    } // end check for empty post
-} // end check if server request is post type 
+                if(!checkDeletions()) // if we're not deleting
+                    checkInsertions();
+            } // 
+    } // 
+} // end 
 
 
 
@@ -112,33 +126,40 @@ return $mysqli;
 }
 
 function drawVideos($mysqli){
-echo "<form action='' method='post'>";
-echo "<table border=1><tr><th>id<th>name<th>category<th>length<th>rented</tr>";
-$sql = "select id,name,category,length,rented from video_inventory";
-$result = $mysqli->query($sql);
-if ($result->num_rows>0)
-    {
-        // output data of each row
-        while($row = $result->fetch_assoc())
-            {
-                echo "<tr><td> " . $row["id"]. "</td>".
-                    "<td>" . $row["name"]. "</td>".
-                    "<td>". $row["category"]. "</td>".
-                    "<td> " . $row["length"]. "</td>";
+    echo "<form action='' method='post'>";
+    echo "<table border=1><tr><th>id<th>name<th>category<th>length<th>rented</tr>";
+    
+// prepare statement
+    $query = "select id,name,category,length,rented from video_inventory";
+    if ($sql=$mysqli->prepare($query)){
+        
+        $sql->execute();
+    
+// bind results
+        $sql->bind_result($id,$name,$category,$length,$rented);
+    
+
+            while($sql->fetch())
+                {
+                    echo "<tr><td> " . $id. "</td>".
+                        "<td>" . $name. "</td>".
+                        "<td>". $category. "</td>".
+                        "<td> " . $length. "</td>";
                     // "<td>" . $row["rented"]. "</tr>";
-                    if ($row["rented"] == 0)
+                    if ($rented == 0)
                         echo "<td>available</td>";
                     else 
                         echo "<td>checked out</td>";
                     echo "<td>";
                     //echo '<input type="submit" name="deleteItem" value=\''.$row[id].'\'>';
-                    echo '<input type="submit" name="'.$row[id].'" value="Delete">';
+                    echo '<input type="submit" name="'.$id.'" value="Delete">';
                     //echo '<td><input type="submit" name="'.$row['id'].'" value="Delete" /></td>"';
                     echo "</td></tr>";
-            }
+                }
     }
-echo "</table></form>";
+    echo "</table></form>";
 }
+
 
 ?>
 
